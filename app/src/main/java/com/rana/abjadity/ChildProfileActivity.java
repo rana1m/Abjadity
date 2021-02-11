06a6c02510aeebfb0df044d3f29f2c908174ddf3
Haifa Hamad <haifahamad1420@gmail.com>
2021-02-11 21:14:33 +0300

    Haifa

--- app/src/main/java/com/rana/abjadity/ChildProfileActivity.java
+++ app/src/main/java/com/rana/abjadity/ChildProfileActivity.java
@@ -1,17 +1,22 @@
 package com.rana.abjadity;
 
+import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 
 import android.app.Activity;
+import android.app.ProgressDialog;
+import android.content.ContentResolver;
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.webkit.MimeTypeMap;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
@@ -19,11 +24,17 @@ import android.widget.Spinner;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
+import com.google.firebase.storage.UploadTask;
 import com.jaredrummler.materialspinner.MaterialSpinner;
 
 public class ChildProfileActivity extends AppCompatActivity {
@@ -42,6 +53,8 @@ public class ChildProfileActivity extends AppCompatActivity {
     View dialogView;
     ImageView profileImg;
     Bundle bundle;
+
+    Uri imageURI;
 
     final int REQUSET_CODE = 2;
 
